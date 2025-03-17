893d311bfe3fa54d38abfe520e28793bd13b1b4d
luisvinicius403 <luisvinicius.dev@gmail.com>
2025-03-17 06:16:48 -0300

    style: add new purple details in page

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -10,7 +10,7 @@ interface SkillCardProps {
 export function SkillCard({ title, description, icon: Icon }: SkillCardProps) {
   return (
     <Card className="p-8 bg-[var(--bg-shade)] hover:border-bottom-[var(--primary-color)] shadow-xl gap-0 max-w-[400px]">
-        <Icon className="w-12 h-12 text-primary bg-white p-3 rounded-xl"/>
+        <Icon className="w-12 h-12 text-[var(--primary-color)] bg-white p-3 rounded-xl"/>
         <h3 className="text-3xl font-bold mt-8">{title}</h3>
         <p className="text-gray-600 text-xl mt-5">{description}</p>
     </Card>
